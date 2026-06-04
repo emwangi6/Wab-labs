2878f163ce02f68cd3c1f80e6fb71f77fd134752
emwangi6 <ephantusmkamau@gmail.com>
2026-06-04 11:52:56 +0300

    lab 2.77 js 101

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let age=20;
+var course ="BBIT";
+const university = "Strathmore University";
+
+//let age=22
+//var course ="BCOM"
+//const university = "KCA University"
+if (true){
+  let time = "11:30"
+  var day = "Monday"
+  const year =2024
+}
+console.log(year)
