dff6997192a03964f430fc2fce964fe15c6d7201
emwangi6 <ephantusmkamau@gmail.com>
2026-06-08 13:03:52 +0300

    Lab 2.8 -js functions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let age=20;
+/*let age=20;
 var course ="BBIT";
 const university = "Strathmore University";
 
@@ -10,4 +10,44 @@ if (true){
   var day = "Monday"
   const year =2024
 }
-console.log(year)
+console.log(year)*/
+
+/**
+ * function that caclute the are of a rectangle
+ * @param {int} height
+ * @param {int} width
+ * @return area
+ */
+
+function calculateArea(height , width)
+{
+  if ( height == null )
+    {
+    return console.log('Height missing !')
+    
+    }
+  else if (width == null )
+    {
+    return console.log('width missing !')
+    }
+  else
+    {
+    let area = height * width
+    return area
+    }
+}
+console.log(calculateArea(20,3))
+console.log(calculateArea(20))
+console.log(calculateArea())
+
+//function expression
+const add = function (number_1 , number_2)
+{
+  return number_1 +  number_2
+}
+console.log(add(4,2))
+
+//arrow Functions
+const multiply = ( x , y ) => x * y;
+
+console.log(multiply (2,3))
